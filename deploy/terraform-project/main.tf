7882735967e2c6adec6916069a6067b54c686830
mrwan-tarek <mrwantarek2010@gmail.com>
2024-10-19 01:28:55 +0300

    after finish

--- deploy/terraform-project/main.tf
+++ deploy/terraform-project/main.tf
@@ -34,6 +34,64 @@ module "workers" {
 
 }
 
+
+# Load Balancer (Application or Network Load Balancer)
+resource "aws_lb" "my_lb" {
+  name               = "my-app-lb"
+  internal           = false  # Set to `true` if you want an internal load balancer
+  load_balancer_type = "application"  # Set to `network` if you prefer NLB
+  security_groups    = [module.sg-create.sg_id]
+  subnets            = [module.vpc.public_subnet_1_id]
+
+  enable_deletion_protection   = false
+  enable_cross_zone_load_balancing = true
+
+  tags = {
+    Name = "my-app-lb"
+  }
+}
+
+# Target Group
+resource "aws_lb_target_group" "my_target_group" {
+  name     = "my-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = module.vpc.vpc_id
+
+  health_check {
+    interval            = 30
+    path                = "/"
+    port                = "traffic-port"
+    protocol            = "HTTP"
+    timeout             = 5
+    unhealthy_threshold = 2
+    healthy_threshold   = 2
+  }
+
+  tags = {
+    Name = "my-target-group"
+  }
+}
+
+# Listener for the Load Balancer
+resource "aws_lb_listener" "my_listener" {
+  load_balancer_arn = aws_lb.my_lb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+    fixed_response {
+      status_code = "200"
+      content_type = "text/plain"
+      message_body = "OK"
+    }
+  }
+}
+
+
+########################################################
+
 resource "local_file" "ansible_inventory" {
   depends_on = [
     module.controller,
